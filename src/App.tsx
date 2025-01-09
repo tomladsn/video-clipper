17b7edd0fc51874cb170c10dfcb68f198a0939ee
tomladson <108412668+tomladsn@users.noreply.github.com>
2025-01-09 23:40:32 +0100

    first one

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import VideoProcessor from './components/VideoProcessor';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <VideoProcessor />
+    </div>
+  );
+}
+
+export default App;
